75e311515feede3c3ab630de8cd24986caa5d2ed
Vedant Agrawal <vedantagrawal524@gmail.com>
2025-04-12 12:37:25 +0530

    CSS and JS logic Added

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const myData = {
+     items: [],
+};
+
+const getData = async () => {
+     const response = await fetch("/data.json", {
+          method: "GET",
+          headers: {
+               Accept: "application/json"
+          }
+     });
+     const jsonResponse = await response.json();
+     myData.items = jsonResponse;
+};
+getData();
+
+const trackOption = document.querySelector(".menu");
+
+const sections = [
+     document.querySelector(".work"),
+     document.querySelector(".play"),
+     document.querySelector(".study"),
+     document.querySelector(".exercise"),
+     document.querySelector(".social"),
+     document.querySelector(".care"),
+];
+
+const timeElements = sections.map(section => {
+     return {
+          section: section,
+          current: section.querySelector(".current-time"),
+          previous: section.querySelector(".previous-time")
+     };
+});
+
+function formatHours(hours) {
+     return hours === 1 ? "1hr" : `${hours}hrs`;
+}
+
+trackOption.addEventListener('click', (event) => {
+     if (!event.target.matches("p")) return;
+
+     document.querySelectorAll(".menu p").forEach((item) =>
+          item.classList.remove("active")
+     );
+
+     event.target.classList.add("active");
+
+     const timeframe = event.target.classList[0];
+
+     for (let i = 0; i < timeElements.length; i++) {
+          timeElements[i].current.textContent = formatHours(myData.items[i].timeframes[timeframe].current);
+          timeElements[i].previous.textContent = formatHours(myData.items[i].timeframes[timeframe].previous);
+     }
+});
